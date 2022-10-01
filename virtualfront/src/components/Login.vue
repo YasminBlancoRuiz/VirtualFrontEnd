77ea452f013ff7b0841191c818c0322bee04b6e8
YasminBlancoRuiz <yazmin-1906@hotmail.com>
2022-09-30 19:07:38 -0500

    Se agrega código del formulario mascota

--- virtualfront/src/components/Login.vue
+++ virtualfront/src/components/Login.vue
@@ -42,7 +42,7 @@ export default {
                 } 
                 
                 this.$emit('completedLogin', dataLogin)  
-                  
+                   
                             
             })
             .catch((error) =>{
